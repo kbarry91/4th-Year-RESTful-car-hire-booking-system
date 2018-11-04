2123fc6e12f4460ace552f1db8bb5ddb6e505c5b
kevin barry <g00339811@gmit.ie>
2018-11-04 18:25:08 +0000

    Added RMI structure

---
+++ WebService/src/main/java/ie/gmit/sw/RMI/FileService.java
@@ -0,0 +1,14 @@
+package ie.gmit.sw.RMI;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+
+public interface FileService extends Remote{
+	
+	public byte[] getFile(String filename) throws RemoteException;
+	
+	public ArrayList<String> getFileNames() throws RemoteException;
+	
+	public void uploadFile(String fileName, byte[] bytes) throws RemoteException;
+}
